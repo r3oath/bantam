69ead0bb4a114724ac01d2338a527a71177edce1
Tristan Strathearn <r3oath@gmail.com>
2015-04-25 20:54:06 +1000

    Added new feature "Role".

---
+++ src/Role.php
@@ -0,0 +1,98 @@
+<?php namespace r3oath\bantam;
+
+// The MIT License (MIT)
+
+// Copyright (c) 2015 Tristan Strathearn
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+require_once 'Prelim.php';
+
+class Role {
+    private static $roles = array();
+
+    public static function create($role) {
+        if(Prelim::strNullOrEmpty($role)) {
+            return false;
+        }
+
+        if(!isset(static::$roles[$role])) {
+            static::$roles[$role] = array();
+        }
+
+        return true;
+    }
+
+    public static function exists($role) {
+        if(Prelim::strNullOrEmpty($role)) {
+            return false;
+        }
+
+        if(isset(static::$roles[$role])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function set($role, $perm) {
+        if(!static::exists($role)) {
+            return false;
+        }
+
+        if(Prelim::strNullOrEmpty($perm)) {
+            return false;
+        }
+
+        if(!isset(static::$roles[$role])) {
+            return false;
+        }
+
+        static::$roles[$role][] = $perm;
+        return true;
+    }
+
+    public static function can($role, $perm) {
+        if(!static::exists($role)) {
+            return false;
+        }
+
+        if(Prelim::strNullOrEmpty($perm)) {
+            return false;
+        }
+
+        if(in_array($perm, static::$roles[$role])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function inherit($role, $from) {
+        if(!static::exists($role) || !static::exists($from)) {
+            return false;
+        }
+
+        foreach (static::$roles[$from] as $perm) {
+            static::set($role, $perm);
+        }
+
+        return true;
+    }
+}
---
+++ tests/units/Role.php
@@ -0,0 +1,63 @@
+<?php namespace r3oath\bantam\tests\units;
+
+include_once 'src/Role.php';
+
+use \mageekguy\atoum;
+use \r3oath\bantam;
+
+class Role extends atoum\test {
+    public function testCreate() {
+        // Edge cases.
+        $this->variable(bantam\Role::create(''))->isEqualTo(false);
+        $this->variable(bantam\Role::create(null))->isEqualTo(false);
+        $this->variable(bantam\Role::create(123))->isEqualTo(false);
+        $this->variable(bantam\Role::exists(''))->isEqualTo(false);
+        $this->variable(bantam\Role::exists(null))->isEqualTo(false);
+        $this->variable(bantam\Role::exists(123))->isEqualTo(false);
+
+        // Normal use.
+        $this->variable(bantam\Role::create('admin'))->isEqualTo(true);
+        $this->variable(bantam\Role::exists('admin'))->isEqualTo(true);
+    }
+
+    public function testSet() {
+        // Setup.
+        bantam\Role::create('admin');
+
+        // Edge cases.
+        $this->variable(bantam\Role::set('admin', ''))->isEqualTo(false);
+        $this->variable(bantam\Role::set('admin', null))->isEqualTo(false);
+        $this->variable(bantam\Role::set('admin', 123))->isEqualTo(false);
+        $this->variable(bantam\Role::can('admin', ''))->isEqualTo(false);
+        $this->variable(bantam\Role::can('admin', null))->isEqualTo(false);
+        $this->variable(bantam\Role::can('admin', 123))->isEqualTo(false);
+
+        // Normal use.
+        $this->variable(bantam\Role::set('admin', 'create'))->isEqualTo(true);
+        $this->variable(bantam\Role::can('admin', 'create'))->isEqualTo(true);
+        $this->variable(bantam\Role::can('admin', 'create123'))->isEqualTo(false);
+    }
+
+    public function testInherit() {
+        // Setup.
+        bantam\Role::create('admin');
+        bantam\Role::create('manager');
+        bantam\Role::set('admin', 'create');
+        bantam\Role::set('manager', 'manage');
+
+        // Edge cases.
+        $this->variable(bantam\Role::inherit('admin', null))->isEqualTo(false);
+        $this->variable(bantam\Role::inherit('admin', ''))->isEqualTo(false);
+        $this->variable(bantam\Role::inherit('admin', 123))->isEqualTo(false);
+        $this->variable(bantam\Role::inherit(null, 'manager'))->isEqualTo(false);
+        $this->variable(bantam\Role::inherit('', 'manager'))->isEqualTo(false);
+        $this->variable(bantam\Role::inherit(123, 'manager'))->isEqualTo(false);
+
+        // Normal use.
+        $this->variable(bantam\Role::inherit('admin', 'manager'))->isEqualTo(true);
+        $this->variable(bantam\Role::can('admin', 'manage'))->isEqualTo(true);
+        $this->variable(bantam\Role::inherit('manager', 'admin'))->isEqualTo(true);
+        $this->variable(bantam\Role::can('manager', 'create'))->isEqualTo(true);
+        $this->variable(bantam\Role::inherit('admin', 'user'))->isEqualTo(false);
+    }
+}
