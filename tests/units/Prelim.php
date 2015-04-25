9634a0d568ea0f57248e7258d0a4785fed0879df
Tristan Strathearn <r3oath@gmail.com>
2015-04-25 20:53:50 +1000

    Test cleanups.

--- tests/units/Prelim.php
+++ tests/units/Prelim.php
@@ -1,6 +1,5 @@
 <?php namespace r3oath\bantam\tests\units;
 
-// require_once '../../mageekguy.atoum.phar';
 include_once 'src/Prelim.php';
 
 use \mageekguy\atoum;
@@ -8,50 +7,44 @@ use \r3oath\bantam;
 
 class Prelim extends atoum\test {
     public function testAllowedKeys() {
-        $arrEmpty = array();
-        $arrNull = null;
-        $arrSimple = array('a' => 0, 'b' => 1, 'c' => 2);
-        $arrNot = 123;
+        $arr = array('a' => 0, 'b' => 1, 'c' => 2);
 
         // Edge cases.
-        $this->variable(bantam\Prelim::allowedKeys($arrEmpty))->isEqualTo(null);
-        $this->variable(bantam\Prelim::allowedKeys($arrNot))->isEqualTo(null);
-        $this->variable(bantam\Prelim::allowedKeys($arrSimple, $arrNot))->isEqualTo(null);
-        $this->variable(bantam\Prelim::allowedKeys($arrEmpty, array('a', 'b')))->isEqualTo(null);
-        $this->variable(bantam\Prelim::allowedKeys($arrNull))->isEqualTo(null);
-        $this->variable(bantam\Prelim::allowedKeys($arrNull, array('a', 'b')))->isEqualTo(null);
-        $this->array(bantam\Prelim::allowedKeys($arrSimple, array('a', 'x')))
+        $this->variable(bantam\Prelim::allowedKeys(array()))->isEqualTo(null);
+        $this->variable(bantam\Prelim::allowedKeys(123))->isEqualTo(null);
+        $this->variable(bantam\Prelim::allowedKeys($arr, 123))->isEqualTo(null);
+        $this->variable(bantam\Prelim::allowedKeys(array(), array('a', 'b')))->isEqualTo(null);
+        $this->variable(bantam\Prelim::allowedKeys(null))->isEqualTo(null);
+        $this->variable(bantam\Prelim::allowedKeys(null, array('a', 'b')))->isEqualTo(null);
+        $this->array(bantam\Prelim::allowedKeys($arr, array('a', 'x')))
             ->integer['a']->isEqualTo(0)
             ->variable['x']->isEqualTo(null);
-        $this->array(bantam\Prelim::allowedKeys($arrSimple, array('y', 'x')))
+        $this->array(bantam\Prelim::allowedKeys($arr, array('y', 'x')))
             ->variable['y']->isEqualTo(null)
             ->variable['x']->isEqualTo(null);
-        $this->array(bantam\Prelim::allowedKeys($arrSimple))->hasKeys(array('a', 'b', 'c'));
+        $this->array(bantam\Prelim::allowedKeys($arr))->hasKeys(array('a', 'b', 'c'));
 
         // Normal use.
-        $this->array(bantam\Prelim::allowedKeys($arrSimple, array('a', 'b', 'c')))->hasKeys(array('a', 'b', 'c'));
-        $this->array(bantam\Prelim::allowedKeys($arrSimple, array('a', 'b', 'c')))
+        $this->array(bantam\Prelim::allowedKeys($arr, array('a', 'b', 'c')))->hasKeys(array('a', 'b', 'c'));
+        $this->array(bantam\Prelim::allowedKeys($arr, array('a', 'b', 'c')))
             ->variable['a']->isEqualTo(0)
             ->variable['b']->isEqualTo(1)
             ->variable['c']->isEqualTo(2);
-        $this->array(bantam\Prelim::allowedKeys($arrSimple, array('a', 'b')))->hasKeys(array('a', 'b'));
-        $this->array(bantam\Prelim::allowedKeys($arrSimple, array('a', 'b')))
+        $this->array(bantam\Prelim::allowedKeys($arr, array('a', 'b')))->hasKeys(array('a', 'b'));
+        $this->array(bantam\Prelim::allowedKeys($arr, array('a', 'b')))
             ->variable['a']->isEqualTo(0)
             ->variable['b']->isEqualTo(1);
     }
 
     public function testHasLength() {
-        $strEmpty = '';
-        $strNull = null;
         $strShort = 'Tristan';
         $strLong = 'This is a pretty long string aye!';
-        $strNot = 123;
 
         // Edge cases.
-        $this->variable(bantam\Prelim::hasLength($strEmpty))->isEqualTo(false);
-        $this->variable(bantam\Prelim::hasLength($strEmpty))->isEqualTo(false);
-        $this->variable(bantam\Prelim::hasLength($strNull))->isEqualTo(false);
-        $this->variable(bantam\Prelim::hasLength($strNot))->isEqualTo(false);
+        $this->variable(bantam\Prelim::hasLength(''))->isEqualTo(false);
+        $this->variable(bantam\Prelim::hasLength(''))->isEqualTo(false);
+        $this->variable(bantam\Prelim::hasLength(null))->isEqualTo(false);
+        $this->variable(bantam\Prelim::hasLength(123))->isEqualTo(false);
         $this->variable(bantam\Prelim::hasLength($strShort, array('min' => 10, 'max' => 5)))->isEqualTo(false);
 
         // Normal use.
@@ -68,16 +61,13 @@ class Prelim extends atoum\test {
     }
 
     public function testHasNulls() {
-        $arrEmpty = array();
-        $arrNull = null;
         $arrWithNull = array('a', 'b', null);
         $arrWithoutNull = array('a', 'b', 'c' => 0);
-        $arrNot = 123;
 
         // Edge cases.
-        $this->variable(bantam\Prelim::hasNulls($arrEmpty))->isEqualTo(false);
-        $this->variable(bantam\Prelim::hasNulls($arrNull))->isEqualTo(true);
-        $this->variable(bantam\Prelim::hasNulls($arrNot))->isEqualTo(true);
+        $this->variable(bantam\Prelim::hasNulls(array()))->isEqualTo(false);
+        $this->variable(bantam\Prelim::hasNulls(null))->isEqualTo(true);
+        $this->variable(bantam\Prelim::hasNulls(123))->isEqualTo(true);
 
         // Normal use.
         $this->variable(bantam\Prelim::hasNulls($arrWithNull))->isEqualTo(true);
@@ -88,12 +78,10 @@ class Prelim extends atoum\test {
         $numericStr = '5';
         $numericInt = 5;
         $numericFloat = 5.0;
-        $numericNull = null;
-        $numericNot = 'abc';
 
         // Edge cases.
-        $this->variable(bantam\Prelim::isNumeric($numericNull))->isEqualTo(false);
-        $this->variable(bantam\Prelim::isNumeric($numericNot))->isEqualTo(false);
+        $this->variable(bantam\Prelim::isNumeric(null))->isEqualTo(false);
+        $this->variable(bantam\Prelim::isNumeric('abc'))->isEqualTo(false);
         $this->variable(bantam\Prelim::isNumeric($numericStr, array('min' => 10, 'max' => 5)))->isEqualTo(false);
 
         // Normal use.
@@ -111,33 +99,27 @@ class Prelim extends atoum\test {
     }
 
     public function testStrNullOrEmpty() {
-        $strEmpty = '';
-        $strNull = null;
         $strSimple = 'Sphinx of black quartz, judge my vow.';
-        $strNot = 123;
 
         // Normal use.
-        $this->variable(bantam\Prelim::strNullOrEmpty($strEmpty))->isEqualTo(true);
-        $this->variable(bantam\Prelim::strNullOrEmpty($strNull))->isEqualTo(true);
-        $this->variable(bantam\Prelim::strNullOrEmpty($strNot))->isEqualTo(true);
+        $this->variable(bantam\Prelim::strNullOrEmpty(''))->isEqualTo(true);
+        $this->variable(bantam\Prelim::strNullOrEmpty(null))->isEqualTo(true);
+        $this->variable(bantam\Prelim::strNullOrEmpty(123))->isEqualTo(true);
         $this->variable(bantam\Prelim::strNullOrEmpty($strSimple))->isEqualTo(false);
     }
 
     public function testStartsWith() {
-        $strEmpty = '';
-        $strNull = null;
         $strSimple = 'Sphinx of black quartz, judge my vow.';
-        $strNot = 123;
 
         // Edge cases.
-        $this->variable(bantam\Prelim::startsWith($strEmpty))->isEqualTo(false);
-        $this->variable(bantam\Prelim::startsWith($strNull))->isEqualTo(false);
+        $this->variable(bantam\Prelim::startsWith(''))->isEqualTo(false);
+        $this->variable(bantam\Prelim::startsWith(null))->isEqualTo(false);
         $this->variable(bantam\Prelim::startsWith($strSimple))->isEqualTo(false);
-        $this->variable(bantam\Prelim::startsWith($strSimple, $strNull))->isEqualTo(false);
-        $this->variable(bantam\Prelim::startsWith($strSimple, $strEmpty))->isEqualTo(false);
-        $this->variable(bantam\Prelim::startsWith($strSimple, $strNot))->isEqualTo(false);
+        $this->variable(bantam\Prelim::startsWith($strSimple, null))->isEqualTo(false);
+        $this->variable(bantam\Prelim::startsWith($strSimple, ''))->isEqualTo(false);
+        $this->variable(bantam\Prelim::startsWith($strSimple, 123))->isEqualTo(false);
         $this->variable(bantam\Prelim::startsWith($strSimple, $strSimple.'extra'))->isEqualTo(false);
-        $this->variable(bantam\Prelim::startsWith($strNot))->isEqualTo(false);
+        $this->variable(bantam\Prelim::startsWith(123))->isEqualTo(false);
 
         // Normal use.
         $this->variable(bantam\Prelim::startsWith($strSimple, 'Sphinx'))->isEqualTo(true);
@@ -147,20 +129,17 @@ class Prelim extends atoum\test {
     }
 
     public function testEndsWith() {
-        $strEmpty = '';
-        $strNull = null;
         $strSimple = 'Sphinx of black quartz, judge my vow.';
-        $strNot = 123;
 
         // Edge cases.
-        $this->variable(bantam\Prelim::endsWith($strEmpty))->isEqualTo(false);
-        $this->variable(bantam\Prelim::endsWith($strNull))->isEqualTo(false);
+        $this->variable(bantam\Prelim::endsWith(''))->isEqualTo(false);
+        $this->variable(bantam\Prelim::endsWith(null))->isEqualTo(false);
         $this->variable(bantam\Prelim::endsWith($strSimple))->isEqualTo(false);
-        $this->variable(bantam\Prelim::endsWith($strSimple, $strNull))->isEqualTo(false);
-        $this->variable(bantam\Prelim::endsWith($strSimple, $strEmpty))->isEqualTo(false);
-        $this->variable(bantam\Prelim::endsWith($strSimple, $strNot))->isEqualTo(false);
+        $this->variable(bantam\Prelim::endsWith($strSimple, null))->isEqualTo(false);
+        $this->variable(bantam\Prelim::endsWith($strSimple, ''))->isEqualTo(false);
+        $this->variable(bantam\Prelim::endsWith($strSimple, 123))->isEqualTo(false);
         $this->variable(bantam\Prelim::endsWith($strSimple, $strSimple.'extra'))->isEqualTo(false);
-        $this->variable(bantam\Prelim::endsWith($strNot))->isEqualTo(false);
+        $this->variable(bantam\Prelim::endsWith(123))->isEqualTo(false);
 
         // Normal use.
         $this->variable(bantam\Prelim::endsWith($strSimple, 'vow.'))->isEqualTo(true);
@@ -171,14 +150,13 @@ class Prelim extends atoum\test {
 
     public function testClamp() {
         $value = 5;
-        $valueNot = 'xyz';
 
         // Edge cases.
         $this->variable(bantam\Prelim::clamp($value))->isEqualTo($value);
         $this->variable(bantam\Prelim::clamp($value, null, null))->isEqualTo($value);
-        $this->variable(bantam\Prelim::clamp($value, $valueNot, $valueNot))->isEqualTo($value);
+        $this->variable(bantam\Prelim::clamp($value, 'xyz', 'xyz'))->isEqualTo($value);
         $this->variable(bantam\Prelim::clamp(null))->isEqualTo(null);
-        $this->variable(bantam\Prelim::clamp($valueNot))->isEqualTo(null);
+        $this->variable(bantam\Prelim::clamp('xyz'))->isEqualTo(null);
 
         // Normal use.
         $this->variable(bantam\Prelim::clamp($value, 0, $value))->isEqualTo($value);
